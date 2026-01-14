3fce05d9517ed015f91ed6d2038c6257a67a5702
Nico Plyley <nico@phoenixtechlab.com>
2026-01-13 19:33:03 -0700

    feat: added bear image to page

--- src/lib/sections/Hero.svelte
+++ src/lib/sections/Hero.svelte
@@ -1,30 +1,30 @@
 <script lang="ts">
+	import { PUBLIC_PARENTS_NAME } from '$env/static/public';
 	import { Container } from '$lib/components';
+	import bear from '$lib/assets/bear.webp';
 </script>
 
-<Container class="flex flex-col items-center justify-center gap-20">
+<Container class="flex flex-col items-center pt-16 lg:pt-28">
 	<!-- Bear -->
 	<div
-		class="flex h-40 w-40 items-center justify-center rounded-full border-4 border-bear-medium bg-white shadow-2xl"
+		class="mb-5 flex h-40 w-40 items-center justify-center overflow-hidden rounded-full border-4 border-bear-medium bg-white shadow-2xl"
 	>
-		Bear Image
+		<img src={bear} alt="Teddy bear" class="w-[90%]" />
 	</div>
 
 	<!-- Name Tag -->
 	<div
-		class="w-full max-w-205 rotate-3 rounded-2xl border-6 border-bear-cream bg-white py-7.5 text-center shadow-2xl"
+		class="mb-7.5 w-full max-w-205 rotate-3 rounded-2xl border-6 border-bear-cream bg-white py-7.5 text-center shadow-2xl"
 	>
 		<h4 class="tracking-wide text-bear-medium uppercase">We Can bearly Wait</h4>
 		<p class="text-sm tracking-wide uppercase">For the arrival of</p>
 		<h1 class="my-8">Piper Joy</h1>
-		<h4 class="text-bear-medium italic">John & Iris Nyquist</h4>
+		<h4 class="text-bear-medium italic">{PUBLIC_PARENTS_NAME || 'John & Jane Doe'}</h4>
 	</div>
 
 	<!-- CTA -->
 	<a
-		class="rounded-full border-2 border-white bg-bear-medium px-10 py-4 font-sans text-lg font-bold text-white shadow-xl transition-transform hover:-translate-y-0.5"
+		class="rounded-full border-2 border-white bg-bear-medium px-10 py-4 font-sans text-lg font-bold text-white shadow-xl transition-transform select-none hover:-translate-y-0.5"
 		href="#rsvp">RSVP Here</a
 	>
-
-	<!-- Info Cards -->
 </Container>
